c0435fcf650d914533027fbf94dc942c2db0c504
OctopusSwellfish <010-2648-8131@daum.net>
2019-05-29 11:21:42 +0000

    검색기능구현

--- nodeServer/routes/mypage.js
+++ nodeServer/routes/mypage.js
@@ -5,15 +5,16 @@ var sequelize = require('../models').sequelize;
 var Medicine = require('../models').Medicine;
 var User = require('../models').User;
 
+var Op = sequelize.Op;
 
 router.post('/showAllmedicine', function(req, res) {
 	
-	var AllMedicine = req.body.AllMedicine;
-	console.log(AllMedicine);
+	var reqStatus = req.body.AllMedicine;
+	console.log(reqStatus);
 		
-
-		Medicine.findAll({
-		attributes: ['name', 'ingredient', 'period', 'effect', 'caution', 'company'], type: sequelize.QueryTypes.SELECT})
+	Medicine.findAll({
+		attributes: { exclude: ['id'] } 
+	})
 	.then(function(resultSet)
 		{
 			var response = {AllMedicine: 'Success',
@@ -30,6 +31,35 @@ router.post('/showAllmedicine', function(req, res) {
 	});
 
 });
+
+router.post('/search', function(req, res) {
+	
+	var keyword = req.body.SearchMedicine;
+	console.log(keyword);
+			
+	/*Medicine.findAll({
+		attribute: { exclude: ['id'] },
+		name: {
+			[Op.substring]: keyword
+		}
+		
+	})*/
+	sequelize.query('select name, ingredient, period, effect, caution, company from medicines where name like \'%'+keyword+'%\'', {/* {replacements: { ID: keyword},*/ type: sequelize.QueryTypes.SELECT})
+	.then(function(resultSet) {
+		var response = {SearchMedicine: 'Success',
+				Search_medi_list: resultSet
+				};
+		
+		res.json(response);
+
+		console.log(response);
+		console.log('response Success!');
+	})
+	.catch(function(err) {
+		console.log('데이터 읽어오기 프로세스 오류 :'+err);
+	});
+});
+
 	
 
 
